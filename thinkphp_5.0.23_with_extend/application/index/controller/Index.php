78076018cf348218bf10a8f2451f003e85a01a8d
782612467@qq.com <lhp782612467>
2019-03-22 11:12:46 +0800

    提交demo

---
+++ thinkphp_5.0.23_with_extend/application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        $arr = array(
+               'id'   => 1,
+               'name' => "卢海平",
+               'sex'  => 18 
+            );
+        var_dump($arr);
+        
+    }
+}
